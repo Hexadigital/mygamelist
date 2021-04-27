f8e0c42231630ffbe929a392ba9c45f61b6955fc
Frostflake <57190472+Frostflake@users.noreply.github.com>
2021-04-27 19:30:31 -0400

    Replace list for tags/genres/platforms with checkboxes

--- mygamelist/games/admin.py
+++ mygamelist/games/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.db.models import ManyToManyField
+from django.forms import CheckboxSelectMultiple
 
 # Register your models here.
 from .models import Game, Genre, Platform, Tag, UserProfile
@@ -13,11 +15,16 @@ class UserProfileInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline,)
 
+class GameAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
+
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
 admin.site.register(Genre)
 admin.site.register(Platform)
 admin.site.register(Tag)
