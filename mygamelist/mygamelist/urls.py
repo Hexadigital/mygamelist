ab1848942c1c20999dc7fdf3ef7167c977d58f92
Frostflake <57190472+Frostflake@users.noreply.github.com>
2021-06-23 22:36:39 -0400

    Remove machina and migrations

--- mygamelist/mygamelist/urls.py
+++ mygamelist/mygamelist/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from machina import urls as machina_urls
 
 urlpatterns = [
     path('', include('games.urls')),
-    path('admin/', admin.site.urls),
-    path('forum/', include(machina_urls))
+    path('admin/', admin.site.urls)
 ]
